82ab40e456b953895cd3d69b4fe1f358b6e1cb64
Lee-Stenio Nazer <ls_nazer@hotmail.com>
2024-01-27 00:44:28 -0500

    requires

--- entity/entity.lua
+++ entity/entity.lua
@@ -1,3 +1,53 @@
-function something()
-  print("Something")
+local vector = require "..\\3rdParty\\vector\\vector"
+local module = {
+  _version = "0.0.1",
+  _description = "base entity",
+  _url = "https://github.com/themousery/vector.lua",
+  _license = [[
+    Copyright (c) 2018 themousery
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+  ]]
+}
+
+-- create the module
+local entity = {}
+entity.__index = entity
+
+-- get a random function from Love2d or base lua, in that order.
+local rand = math.random
+if love and love.math then rand = love.math.random end
+  
+-- makes a new entity
+local function new(x,y)
+  return setmetatable({pos=pos or vector.new(x,y)}, entity)
 end
+local function new(pos)
+  return setmetatable({pos=pos or vector.new(0,0)}, entity)
+end
+
+
+
+
+-- pack up and return module
+module.new = new
+--module.random = random
+--module.fromAngle = fromAngle
+--module.isvector = isvector
+return setmetatable(module, {__call = function(_,...) return new(...) end})
--- main.lua
+++ main.lua
@@ -1,8 +1,54 @@
+--Store path to variable
+
+local startingPackagePath = package.path
+
+--Append to path
+--package.path = currentDir .. "\\3rdParty\\tick;" ..package.path
+--package.path = currentDir .. "\\3rdParty\\log.lua\\log;" ..package.path
+--package.path = "D:\\Documents\\Projects\\SuperPunchBlock\\3rdParty;" ..package.path
+
+
+--Add packages
+--local open = io.open
+
+--log = require "3rdParty/log.lua/log"
+--tick = require "3rdParty/tick/tick"
+--log = require "log.lua"
+--tick = require "tick"
+--local lfs = require "lfs"
+--print(lfs.currentdir())
+--entity = require "entity.entity"
+
+--Restore path
+--package.path = startingPackagePath
+
+--https://love2d.org/wiki/Category:Snippets
+--https://love2d.org/wiki/License
+--https://love2d.org/wiki/Game_Distribution
+--https://love2d.org/wiki/Category:Tutorials
+--https://www.love2d.org/wiki/love.filesystem.setRequirePath
+--https://create.roblox.com/docs/luau/metatables
 
 function love.load()
-  pl_entity = require ("entity/entity")
   tick = require("3rdParty/tick/tick")
+  log = require("3rdParty/log/log")
+  vector = require("3rdParty/vector/vector")
+  entity = require("entity.entity")
+
+
+  local curDir = love.filesystem.getWorkingDirectory()
+  log.info(curDir)
+  local curDir = love.filesystem.getSource()
+  print(curDir)
+  log.info(curDir)
+  love.filesystem.setRequirePath( curDir )
+  --love.filesystem.setRequirePath( curDir .. '3rdParty\\log.lua\\' )
+
+  --open = require("io.open")
+  --log.outfile = "output/log.txt"
+  log.info("Loading Love Module")
   
+  vec = vector.new(10,10)
   Message = "Hello from LOVE"
   local secret = 'This is a local string' -- local variable, apostrophes on the string.
   --something()
@@ -15,7 +61,7 @@ function love.update(dt)
   tick.update(dt)
   --print(dt)
   --TODO
-  print("Time: ", dt)
+  --print("Time: ", dt)
 end
 
 
